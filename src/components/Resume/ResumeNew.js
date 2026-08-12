a2a5524c7a2980bc99c79642530163a8d903aaaf
Your Name <Sajidagoat@gmail.com>
2026-08-11 23:58:36 -0400

    fixed bugs

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Sajid Amin Latest Resume 8_7_26.docx.pdf";
+import pdf from "../../Assets/Sajid_Amin_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import Reveal from "../Reveal";
